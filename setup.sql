da006a265d11be40d319d562944194fadfd5add7
BHupen <024neb952@sxc.edu.np>
2026-02-16 15:37:16 +0545

    Add files via upload
    the file for the mysql setup

---
+++ setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE omni_learn;
+
+USE omni_learn;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    age INT NOT NULL,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
